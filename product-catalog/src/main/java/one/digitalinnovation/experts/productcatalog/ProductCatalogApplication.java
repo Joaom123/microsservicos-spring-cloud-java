0a771b0fc2a7d6f5f5601c0492193f7c0aaa36ec
João Marcus <joaomarcus12769@gmail.com>
2021-06-16 02:30:11 -0300

    config server to startup cartapp and ProductCatalogApplication on ports 8082 and 8081 respectively

--- product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/ProductCatalogApplication.java
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/ProductCatalogApplication.java
@@ -1,7 +1,9 @@
 package one.digitalinnovation.experts.productcatalog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class ProductCatalogApplication {
